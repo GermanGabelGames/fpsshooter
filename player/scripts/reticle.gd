02295648cb3815f655f1f08e27a9fdd2376ebe60
GermanGabelGames <tesler408@gmail.com>
2025-03-20 20:42:13 +0100

    2025-03-20-20:42
    crosshair

---
+++ player/scripts/reticle.gd
@@ -0,0 +1,20 @@
+extends CenterContainer
+
+@export var DOT_RADIUS : float = 1.0
+@export var DOT_COLOR : Color = Color.WHITE
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	queue_redraw()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _draw():
+	draw_circle(Vector2(0,0),DOT_RADIUS,DOT_COLOR)
+	draw_line(Vector2(8,0),Vector2(16,0),DOT_COLOR,2)
+	draw_line(Vector2(-8,0),Vector2(-16,0),DOT_COLOR,2)
+	draw_line(Vector2(0,8),Vector2(0,16),DOT_COLOR,2)
+	draw_line(Vector2(0,-8),Vector2(0,-16),DOT_COLOR,2)
